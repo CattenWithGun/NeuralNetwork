4ad8b60e5df6f76b5ae94f35730e207379ab0bc7
Catten <cattengood@gmail.com>
2024-10-02 20:16:31 -0700

    README.md created

--- Program.cs
+++ Program.cs
@@ -5,86 +5,86 @@ using UserPrompts;
 
 public class Program
 {
-	public static void Main()
-	{
-		//Creates a list to store networks and their names
-		List<NeuralNetwork> networks = new List<NeuralNetwork>();
-		List<string> networkNames = new List<string>();
+    public static void Main()
+    {
+        //Creates a list to store networks and their names
+        List<NeuralNetwork> networks = new List<NeuralNetwork>();
+        List<string> networkNames = new List<string>();
 
-		//Stores file paths, will change later
-		string labelsFilePath = "/home/catten/Desktop/MNIST_Train_Database/train-labels.idx1-ubyte";
-		string imagesFilePath = "/home/catten/Desktop/MNIST_Train_Database/train-images.idx3-ubyte";
-		string filePathToStoreIn = "/home/catten/Desktop/network.txt";
+        //Stores file paths, will change later
+        string labelsFilePath = "/home/catten/Desktop/MNIST_Train_Database/train-labels.idx1-ubyte";
+        string imagesFilePath = "/home/catten/Desktop/MNIST_Train_Database/train-images.idx3-ubyte";
+        string filePathToStoreIn = "/home/catten/Desktop/network.txt";
 
-		NeuralNetwork? nullableNetwork;
-		NeuralNetwork network;
+        NeuralNetwork? nullableNetwork;
+        NeuralNetwork network;
 
-		Console.Clear();
-		Commands.ShowCommands();
-		while (true)
-		{
-			string command = Prompts.PromptUntilConditionMet("\nEnter: ", "\nNot a command, enter again: ", Checks.IsOption, "");
-			//Finds the command and executes it
-			switch (command)
-			{
-				case "show":
-					Commands.ShowNetworks(networks);
-					break;
-				case "train":
-					nullableNetwork = GetNetwork(networks, networkNames, "Enter a network to train: ", "Network wasn't in the saved networks list\nEnter again: ");
-					if (nullableNetwork == null) continue;
-					network = nullableNetwork;
-					Commands.Train(network, labelsFilePath, imagesFilePath);
-					break;
-				case "test":
-					Commands.Test(networks, networkNames, labelsFilePath, imagesFilePath);
-					break;
-				case "error":
-					nullableNetwork = GetNetwork(networks, networkNames, "Enter a network to get the error of: ", "Network wasn't in the saved networks list\nEnter again: ");
-					if (nullableNetwork == null) continue;
-					network = nullableNetwork;
-					Commands.PrintError(network, labelsFilePath, imagesFilePath);
-					break;
-				case "store":
-					Commands.Store(networks, networkNames, filePathToStoreIn);
-					break;
-				case "make":
-					Commands.Make(networks, networkNames);
-					break;
-				case "delete":
-					Commands.Delete(networks, networkNames);
-					break;
-				case "clear":
-					Console.Clear();
-					break;
-				case "help":
-					Commands.ShowCommands();
-					break;
-			}
-		}
-	}
+        Console.Clear();
+        Commands.ShowCommands();
+        while(true)
+        {
+            string command = Prompts.PromptUntilConditionMet("\nEnter: ", "\nNot a command, enter again: ", Checks.IsOption, "");
+            //Finds the command and executes it
+            switch(command)
+            {
+                case "show":
+                    Commands.ShowNetworks(networks);
+                    break;
+                case "train":
+                    nullableNetwork = GetNetwork(networks, networkNames, "Enter a network to train: ", "Network wasn't in the saved networks list\nEnter again: ");
+                    if(nullableNetwork == null) continue;
+                    network = nullableNetwork;
+                    Commands.Train(network, labelsFilePath, imagesFilePath);
+                    break;
+                case "test":
+                    Commands.Test(networks, networkNames, labelsFilePath, imagesFilePath);
+                    break;
+                case "error":
+                    nullableNetwork = GetNetwork(networks, networkNames, "Enter a network to get the error of: ", "Network wasn't in the saved networks list\nEnter again: ");
+                    if(nullableNetwork == null) continue;
+                    network = nullableNetwork;
+                    Commands.PrintError(network, labelsFilePath, imagesFilePath);
+                    break;
+                case "store":
+                    Commands.Store(networks, networkNames, filePathToStoreIn);
+                    break;
+                case "make":
+                    Commands.Make(networks, networkNames);
+                    break;
+                case "delete":
+                    Commands.Delete(networks, networkNames);
+                    break;
+                case "clear":
+                    Console.Clear();
+                    break;
+                case "help":
+                    Commands.ShowCommands();
+                    break;
+            }
+        }
+    }
 
-	public static NeuralNetwork? GetNetwork(List<NeuralNetwork> networks, List<string> networkNames, string startingMessage, string problemMessage)
-	{
-		if (networks.Count == 0)
-		{
-			Console.WriteLine("Network list empty");
-			return null;
-		}
+    public static NeuralNetwork? GetNetwork(List<NeuralNetwork> networks, List<string> networkNames, string startingMessage, string problemMessage)
+    {
+        if(networks.Count == 0)
+        {
+            Console.WriteLine("Network list empty");
+            return null;
+        }
 
-		//Get the name of the network
-		string name = Prompts.PromptUntilConditionMet(startingMessage, problemMessage, Checks.NetworkListContainsName, "", networkNames);
-		if (name == "exit") return null;
+        //Get the name of the network
+        string name = Prompts.PromptUntilConditionMet(startingMessage, problemMessage, Checks.NetworkListContainsName, "", networkNames);
+        if(name == "exit") return null;
 
-		//Find the network in the list and return it
-		for (int i = 0; i < networks.Count; i++)
-		{
-			if (networks[i].name == name)
-			{
-				return networks[i];
-			}
-		}
+        //Find the network in the list and return it
+        for(int i = 0; i < networks.Count; i++)
+        {
+            if(networks[i].name == name)
+            {
+                return networks[i];
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+This is a C# console app, and relies on the Newtonsoft.Json NuGet package for storing and reading network files.
+It has hard coded file path values as of 10/2/2024 (Located in the Program.cs file), which point towards the MNIST database files which you will need to store locally on your computer if you want to use this for some reason.
+I'm trying to implement backpropagation to train a neural network to be able to recognize images from the MNIST database as accurately as possible.
+As of right now, it is borked. (And possibly will be for a while)
+
+This repository has had some major changes from the last (now archived) "Detectorinator" repository, and since there have been so many changes, additions, and days forgored to commit changes,
+I made this updated repository and archived the last one.
+I also have Github connected to VSCode now, so Github should be able to see all my changes in (mostly) real time, instead of every other year when I remember Github exists.
