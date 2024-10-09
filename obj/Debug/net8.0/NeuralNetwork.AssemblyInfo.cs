90f4ac07dda3744df03973a9711de681bb587ac0
Catten <cattengood@gmail.com>
2024-10-08 20:14:41 -0700

    Finally done making a github mess, for now.

--- obj/Debug/net8.0/NeuralNetwork.AssemblyInfo.cs
+++ obj/Debug/net8.0/NeuralNetwork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeuralNetwork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995171077967c65d44ac916974ac0456539376a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d2966d4a01b5b7f0bfed8e97e33e2814509940")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeuralNetwork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeuralNetwork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
